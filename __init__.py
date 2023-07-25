12b8db833b47e1a91ba092d0d6fbf84931ded0bd
ZHO-ZHO-ZHO <140084057+ZHO-ZHO-ZHO@users.noreply.github.com>
2023-07-26 00:34:49 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,12 +1,9 @@
 from .pysssss import init
 
 if init(check_imports=["onnxruntime"]):
-    from .wd14tagger import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
-    __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
-
-NODE_CLASS_MAPPINGS = {
-    "WD14Tagger|pysssss": WD14Tagger,
-}
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "WD14Tagger|pysssss": "WD14 Tagger 🐍",
-}
+    NODE_CLASS_MAPPINGS = {
+        "WD14Tagger|pysssss": WD14Tagger,
+    }
+    NODE_DISPLAY_NAME_MAPPINGS = {
+        "WD14Tagger|pysssss": "WD14 Tagger 🐍",
+    }
